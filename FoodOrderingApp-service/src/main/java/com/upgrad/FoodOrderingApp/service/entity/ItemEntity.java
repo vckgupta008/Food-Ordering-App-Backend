c32d08a377a93b0db4e07b53d50fc7d3552e5608
Vicky Gupta <vicky.gupta@in.imshealth.com>
2021-05-01 22:35:43 +0530

    RestEndPoint3 Adding Item entity

---
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/ItemEntity.java
@@ -0,0 +1,71 @@
+package com.upgrad.FoodOrderingApp.service.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "item")
+public class ItemEntity {
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "UUID")
+    @NotNull
+    private String uuid;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getItem_name() {
+        return item_name;
+    }
+
+    public void setItem_name(String item_name) {
+        this.item_name = item_name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Column(name = "ITEM_NAME")
+    @NotNull
+    private String item_name;
+
+    @Column(name = "PRICE")
+    @NotNull
+    private String price;
+
+    @Column(name = "TYPE")
+    @NotNull
+    private String type;
+
+}
