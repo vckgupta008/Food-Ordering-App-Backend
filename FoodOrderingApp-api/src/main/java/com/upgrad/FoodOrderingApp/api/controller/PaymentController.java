b63b11d772edebb4e8a47c4a9f8b751a6df7cfec
Shilpa1193 <indoliashilpa11@gmail.com>
2021-05-05 22:24:47 +0530

    Feature implemeted where apiEndpoint is used to retrieve all the payment method in the databaseordered by their names

---
+++ FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/PaymentController.java
@@ -0,0 +1,47 @@
+package com.upgrad.FoodOrderingApp.api.controller;
+
+import com.upgrad.FoodOrderingApp.api.model.PaymentListResponse;
+import com.upgrad.FoodOrderingApp.api.model.PaymentResponse;
+import com.upgrad.FoodOrderingApp.service.businness.PaymentService;
+import com.upgrad.FoodOrderingApp.service.entity.PaymentEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+public class PaymentController {
+
+    @Autowired
+    private PaymentService paymentService;
+
+
+    /**
+     * This api endpoint is used to retrieve all the payment method present in the database, ordered by their name
+     *
+     * @return ResponseEntity<PaymentListResponse> type object along with HttpStatus OK
+     */
+
+    //Lists all available payment methods
+    //No API input
+    @RequestMapping(value = "/payment", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<PaymentListResponse> getPaymentMethods() {
+        List<PaymentEntity> paymentEntityList = paymentService.getAllPaymentMethods();
+
+        PaymentListResponse paymentListResponse = new PaymentListResponse();
+        for(PaymentEntity payment : paymentEntityList){
+            PaymentResponse paymentResponse = new PaymentResponse();
+            paymentResponse.setId(UUID.fromString(payment.getUuid()));
+            paymentResponse.setPaymentName(payment.getPaymentName());
+            paymentListResponse.addPaymentMethodsItem(paymentResponse);
+        }
+
+        return new ResponseEntity<PaymentListResponse>(paymentListResponse, HttpStatus.OK);
+    }
+}
